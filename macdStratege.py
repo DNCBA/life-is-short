e2c5bdc8b3cf950395a3d00c9eb8e0a37defb638
dncba <354710644@qq.com>
2020-02-17 14:15:36 +0800

    update

--- macdStratege.py
+++ macdStratege.py
@@ -12,6 +12,13 @@ def callWxInfo(topic,price):
     pusher = 'http://wxpusher.zjiecode.com/api/send/message/?appToken=AT_uWm4moKaCgKCGNrOUsCEbBI1A5IrElpx&uid=UID_KUJzRO0bmJxZCWXsIRmUsRX2Zsv3&content=' + info
     requests.get(pusher)
 
+
+def callLastTimtInfo(endTime):
+    baseHost = 'https://www.okex.me'
+    queryCandlesURI = '/api/spot/v3/instruments/BTC-USDT/candles?granularity=86400&end=%sT16:30:01.000Z' % (endTime)
+    responseData = requests.get(baseHost+queryCandlesURI).json()
+    print(responseData[0][0])
+
 def caculateOperation(endTime):
     # step0:采集原始数据
     # 数据格式:time/open/high/low/close/volume
@@ -60,16 +67,21 @@ def caculateOperation(endTime):
 while(1):
     # 8点和20点执行一次
     nowHour = str(datetime.datetime.now())[11:13]
-    print('job执行当前时间'+nowHour)
-    if (nowHour == '23'):
+    callLastTimtInfo(str(datetime.datetime.now())[0:10])
+    print('job执行当前时间'+str(datetime.datetime.now()))
+    if ( nowHour == '01'):
+        caculateOperation(str(datetime.datetime.now())[0:10])
+    if (nowHour == '08'):
         caculateOperation(str(datetime.datetime.now())[0:10])
     if ( nowHour == '20'):
         caculateOperation(str(datetime.datetime.now())[0:10])
     time.sleep(3600)
-    
 
 
-    
+
+
+
+
 
 
 
